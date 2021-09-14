2e3f4e5904ee5346b995ab9189605e80eec23192
danilogabriels <81102193+danilogabriels@users.noreply.github.com>
2021-09-14 14:17:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const router = require('./src/routes/index.js');
+const Seed = require('./src/seeders/index.js');
 
+const app = express();
+
+app.use(router);
+
+app.use(express.static('public'));
+
+app.set('view engine', 'njk');
+
+nunjucks.configure('src/views', {
+  express: app,
+  autoescape: true,
+  noCache: true,
+});
+
+Seed.up();
+
+app.listen(3000, () => {
+  console.log('Fashion App is running!');
+});
